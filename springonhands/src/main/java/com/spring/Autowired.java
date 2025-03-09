9218d2c4e45179800db208657cd7ce93ec057ebd
lemonliu1993 <2305247091@qq.com>
2025-03-09 16:16:15 +0800

    手写Spring——Spring 依赖注入简单实现

---
+++ springonhands/src/main/java/com/spring/Autowired.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by lemoon on 2025/3/2 21:33
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD,ElementType.FIELD})
+public @interface Autowired {
+
+}
--- springonhands/src/main/java/com/spring/LemonApplicationContext.java
+++ springonhands/src/main/java/com/spring/LemonApplicationContext.java
@@ -2,7 +2,11 @@ package com.spring;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by lemoon on 2025/3/2 21:19
@@ -11,16 +15,69 @@ public class LemonApplicationContext {
 
     private Class configClass;
 
+    private ConcurrentHashMap<String,Object> singletonObjects = new ConcurrentHashMap<String, Object>();    //单例池
+    private ConcurrentHashMap<String,BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>();
+
     public LemonApplicationContext(Class configClass) {
         this.configClass = configClass;
 
+        //解析配置类
+        //ComponentScan注解--->扫描路径 --->扫描--->Beandefinition -->BeanDefinitionMap
+        scan(configClass);
+
+        for(Map.Entry<String,BeanDefinition> entry :beanDefinitionMap.entrySet()){
+            String beanName = entry.getKey();
+            BeanDefinition beanDefinition = entry.getValue();
+
+            if(beanDefinition.getScope().equals("singleton")){
+                Object bean = createBean(beanDefinition);//单例Bean
+                singletonObjects.put(beanName,bean);
+
+            }
+        }
+    }
+
+
+    public Object createBean(BeanDefinition beanDefinition){
+        Class clazz = beanDefinition.getClazz();
+        try {
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+
+            //依赖注入
+
+            for(Field declaredField : clazz.getDeclaredFields()){
+                if(declaredField.isAnnotationPresent(Autowired.class)){
+
+                    //Spring 在给属性赋值时，会根据属性的信息从容器里找一个值
+                    Object bean = getBean(declaredField.getName());
+
+                    declaredField.setAccessible(true);
+                    declaredField.set(instance,bean);
+                    //如果此时单例池里还没有对应的bean怎么办？
+                }
+            }
+
+            return instance;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private void scan(Class configClass) {
         //在构造方法中做什么事呢？
         //解析配置类
         //ComponentScan注解 -->扫描路径 -->扫描
 
         ComponentScan componentScanAnnotation = (ComponentScan)configClass.getDeclaredAnnotation(ComponentScan.class);
         String path = componentScanAnnotation.value();  //扫描路径
-        System.out.println(path);
         path = path.replace(".","/");
 
 
@@ -35,8 +92,6 @@ public class LemonApplicationContext {
             File[] files = file.listFiles();
 
             for(File f: files){
-                System.out.println(f);
-                System.out.println(f.getAbsolutePath());
                 String fileName = f.getAbsolutePath();
 
                 if(fileName.endsWith(".class")){
@@ -52,6 +107,25 @@ public class LemonApplicationContext {
                     try {
                         Class<?> clazz = classLoader.loadClass(classNameReplace);
                         if (clazz.isAnnotationPresent(Component.class)) {
+                            //表示当前这个类是一个Bean
+                            //解析类，判断当前bean是单例bean，还是prototype的bean，此时不考虑懒加载了
+                            //BeanDefinition
+
+                            //解析类--->BeanDefinition
+                            Component componentAnnotation = clazz.getDeclaredAnnotation(Component.class);
+                            String beanName = componentAnnotation.value();
+
+                            BeanDefinition beanDefinition = new BeanDefinition();
+
+                            if(clazz.isAnnotationPresent(Scope.class)){
+                                Scope scopeAnnotation = clazz.getDeclaredAnnotation(Scope.class);
+                                beanDefinition.setScope(scopeAnnotation.value());
+                            }else{
+                                beanDefinition.setScope("singleton");
+                            }
+                            beanDefinition.setClazz(clazz);
+
+                            beanDefinitionMap.put(beanName,beanDefinition);
 
                         }
                     } catch (ClassNotFoundException e) {
@@ -62,11 +136,24 @@ public class LemonApplicationContext {
 
             }
         }
-
     }
 
     public Object getBean(String beanName){
-        return null;
+        if(beanDefinitionMap.containsKey(beanName)){
+            BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+            if(beanDefinition.getScope().equals("singleton")){
+                Object o = singletonObjects.get(beanName);
+                return o;
+            }else{
+                //创建Bean对象
+                Object bean = createBean(beanDefinition);
+                return bean;
+
+            }
+        }else{
+            //不存在对应的Bean
+            throw new NullPointerException();
+        }
     }
 
 }
